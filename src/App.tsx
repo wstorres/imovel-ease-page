4a3bb926f0c97c2cd43f265914ed34027df7ff90
Wagner Torres <wav_torres@hotmail.com>
2026-02-08 12:10:12 -0300

    Remove basename from HashRouter component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
